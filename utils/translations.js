65a69878a0e9211df84b051855615f0c879b7e4b
GauravBiraris <136731857+GauravBiraris@users.noreply.github.com>
2026-06-21 17:01:55 +0530

    Add translations for English and Marathi languages

---
+++ utils/translations.js
@@ -0,0 +1,25 @@
+
+export const translations = {
+  en: {
+    logHarvest: "Log Harvest",
+    qrScan: "QR Scan",
+    cancelScanning: "Cancel Scanning",
+    cutComplete: "CUT COMPLETE",
+    gettingLocation: "Getting location...",
+    enterOrScan: "Please enter or scan a ",
+    cutLogged: "Cut logged locally for",
+    failedToSave: "Failed to save data on device.",
+    logging: "Logging..."
+  },
+  mr: {
+    logHarvest: "तोडणी नोंदवा",
+    qrScan: "QR स्कॅन",
+    cancelScanning: "स्कॅनिंग रद्द करा",
+    cutComplete: "तोडणी पूर्ण",
+    gettingLocation: "स्थान मिळवत आहे...",
+    enterOrScan: "कृपया प्रविष्ट करा किंवा स्कॅन करा: ",
+    cutLogged: "नोंदणी यशस्वी: ",
+    failedToSave: "डिव्हाइसवर डेटा जतन करण्यात अयशस्वी.",
+    logging: "नोंदणी करत आहे..."
+  }
+};
